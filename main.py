307b91964bd67de67311859e605cd0a7c14229db
tanayvaswani <vaswani.tanay9@gmail.com>
2024-07-08 04:01:53 +0530

    chore: 08 - update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from kidney_dl.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidney_dl import logger
 
-logger.info("Custom log check!")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
