304ab1bd6897cff47c8cf4679e02c76f585dfc0a
tanayvaswani <vaswani.tanay9@gmail.com>
2024-07-08 19:28:06 +0530

    fix: main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n\n\nx==========x")
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
