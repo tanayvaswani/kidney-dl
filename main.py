6eea3cf44d5a856cb391f50b647992c52484c69e
tanayvaswani <vaswani.tanay9@gmail.com>
2024-07-08 19:25:07 +0530

    feat(module): 08 - update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kidney_dl.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from kidney_dl.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from kidney_dl import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -8,6 +9,18 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
